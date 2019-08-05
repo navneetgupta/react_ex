06f615bde9f4fdcc29924d734608dd958845b699
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-05 20:26:07 +0400

    lecture 36 - working with state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person.js';
 
-function App() {
-    return ( < div className = 'App' >
-        <
-        Person name = "Max"
-        age = "29" / >
-        <
-        Person name = "Manu"
-        age = "12" > My Hobbies: Cycling < /Person> <
-        Person name = "MaxManu"
-        age = "31" / >
-        <
-        /div>
-    );
+class App extends Component {
+    state = {
+        persons: [
+            { name: "Max", age: 29 },
+            { name: "Manu", age: 28 },
+            { name: "MaxManu", age: 26 }
+        ]
+    };
+
+    render() {
+        return ( < div className = 'App' >
+            <
+            Person name = { this.state.persons[0].name } age = { this.state.persons[0].age }
+            / > <
+            Person name = { this.state.persons[1].name } age = { this.state.persons[1].age }
+            / > <
+            Person name = { this.state.persons[2].name } age = { this.state.persons[2].age }
+            / > < /
+            div >
+        );
+    }
 }
+
 
 export default App;
