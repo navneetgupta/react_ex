cb3eeed8a0884283ced83a3d83df02de37b7bf35
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-06 23:13:31 +0400

    Lecture 59: Flexible lists

--- src/App.js
+++ src/App.js
@@ -13,14 +13,22 @@ class App extends Component {
 	showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+  nameChangedHandler = (event, id) => {
+	  const personIndex = this.state.persons.findIndex(p => {
+		  return p.id === id;
+	  });
+	  const person = {
+		  ...this.state.persons[personIndex]
+	  };
+	  // or another way to copy
+	  // const person = Object.assign({}, this.state.persons[personIndex]);
+	  //
+	  person.name = event.target.value;
+	  const persons = [...this.state.persons]
+	  persons[personIndex] = person;
+	  this.setState({
+	      persons: persons
+	  })
   }
 
   togglePersonViewHandler = () => {
@@ -54,6 +62,7 @@ class App extends Component {
 			  	{this.state.persons.map((p,index) => {
 					return <Person
 					click={this.deletePersonElement.bind(this, index)}
+					changed={(event) => this.nameChangedHandler(event, p.id)}
 	  				name={p.name}
 	  				age={p.age}
 					key={p.id} />  // we can pass index as unique element but it depends on list and will keep changing based on the list so its not a good choice
