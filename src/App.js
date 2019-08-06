b5585c183626f7fc1f3e8070622e9581b9e1eea8
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-06 20:35:46 +0400

    Assignment Section 3

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+import UserInput from './Assignment/UserInput';
+import UserOutput from './Assignment/UserOutput';
 
 class App extends Component {
   state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value'
+    username: "Navneet"
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  usernameHandler = (event) => {
     this.setState( {
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    } )
-  }
-
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
+      username: event.target.value
     } )
   }
   /*
@@ -49,22 +29,19 @@ class App extends Component {
 	  };
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-			style={style}
-			onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+	  	<h1>Please Enter Your Username:</h1>
+		<UserInput
+		changed={this.usernameHandler}
+		username={this.state.username}
+		/>
+	  	<UserOutput
+		name={this.state.username}
+		salute = "Hi"
+		p2="How are u Doing today??" />
+		<UserOutput
+		name={this.state.username}
+		salute = "Hi"
+		p2="How are u Doing today??" />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
