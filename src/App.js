780dfa6f52acad7d33457a5139d0934f2a57cf0c
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-06 08:03:21 +0400

    comments added

--- src/App.js
+++ src/App.js
@@ -39,17 +39,17 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button> // this can be inefficient
+        <Person
+          name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
-        <Person 
-          name={this.state.persons[1].name} 
+        <Person
+          name={this.state.persons[1].name}
           age={this.state.persons[1].age}
           click={this.switchNameHandler.bind(this, 'Max!')}
           changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-        <Person 
-          name={this.state.persons[2].name} 
+        <Person
+          name={this.state.persons[2].name}
           age={this.state.persons[2].age} />
       </div>
     );
