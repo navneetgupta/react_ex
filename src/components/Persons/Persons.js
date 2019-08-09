17c578322175608a3b130ff2c40697aa91cc2800
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-09 23:25:48 +0400

    Lecture 89: Component update lifecycle hooks-1

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import Person from './Person/Person';
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+import React, { Component } from "react";
+import Person from "./Person/Person";
+import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 
-const persons = (props) => {
-	let person = null;
-	if(props.showPersons) {
-		person = (
-			<div>
-				{props.persons.map((p,index) => {
-				  return <ErrorBoundary key={p.id} ><Person
-				  click={() => props.clicked(index)}
-				  changed={(event) => props.changed(event, p.id)}
-				  name={p.name}
-				  age={p.age}
-				  /></ErrorBoundary>  // we can pass index as unique element but it depends on list and will keep changing based on the list so its not a good choice
-			  })}
-			</div>
-		);
-	}
-	console.log("[Persons.js] rendering..")
-	return (<div>{person}</div>);
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("[Persons.js] getDerivedStateFromProps..");
+  //   return state;
+  // }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] gettingSnapshots", prevProps, prevState);
+    return null;
+  }
+  render() {
+    let person = null;
+    if (this.props.showPersons) {
+      person = (
+        <div>
+          {this.props.persons.map((p, index) => {
+            return (
+              <ErrorBoundary key={p.id}>
+                <Person
+                  click={() => this.props.clicked(index)}
+                  changed={event => this.props.changed(event, p.id)}
+                  name={p.name}
+                  age={p.age}
+                />
+              </ErrorBoundary>
+            ); // we can pass index as unique element but it depends on list and will keep changing based on the list so its not a good choice
+          })}
+        </div>
+      );
+    }
+    console.log("[Persons.js] rendering..");
+    return <div>{person}</div>;
+  }
+
+  componentDidUpdate() {
+    console.log("[Persons.js] componentDidUpdate");
+  }
 }
 
-export default persons;
+export default Persons;
