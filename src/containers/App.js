1f9110627968da76189746a13fd28952ca187e14
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-13 08:56:55 +0400

    Lecture 106: Setting sTate correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
     ],
     otherState: "some other value",
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
   static getDerivedStateFromProps(props, state) {
     console.log("[App.js] getDerivedStateFromProps");
@@ -38,8 +39,11 @@ class App extends Component {
     person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({
-      persons: persons
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter
+      };
     });
   };
 
