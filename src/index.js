f0c4b743919c5804cd1e4501e8f3798bfa264fa1
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-17 12:45:38 +0400

    Lecture 201. Creating and Using Axios Instances

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.common["Authorization"] = "AUTH_TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 axios.interceptors.request.use(
   config => {
