7dcb8ade7346239a50046e29ca7140ab0a924f75
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-19 21:04:03 +0400

    Lecture 219. Preparing the Project For Routing

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import axios from "../../../axios";
+import Post from "../../../components/Post/Post";
+
+class Posts extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 6).map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
+        this.setState({ posts: posts });
+      })
+      .catch(error => {
+        console.log(error);
+        // this.setState({ error: true });
+      });
+  }
+
+  postselectedHandler = id => {
+    this.setState({
+      selectedPostId: id
+    });
+  };
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something Went Wrong!!!!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            title={post.title}
+            key={post.id}
+            author={post.author}
+            clicked={() => this.postselectedHandler(post.id)}
+          />
+        );
+      });
+    }
+
+    return <section className="Posts"> {posts}</section>;
+  }
+}
+
+export default Posts;
