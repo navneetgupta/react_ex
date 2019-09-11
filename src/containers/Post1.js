138e5ea2853faad6128d7ed345cb4dae75908832
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-09-11 22:03:52 +0400

    Unknown files later added

---
+++ src/containers/Post1.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Post1 = props => <h1> Post Home!!</h1>;
+
+export default Post1;
---
+++ src/containers/User1.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const User1 = props => <h1> User1 Home!!</h1>;
+
+export default User1;
